9903faf6abe3ff8b2273d22e0a413a9e0888b2e9
Simon Zerisenay <81813787+simon-zerisenay@users.noreply.github.com>
2023-04-08 13:32:34 +0400

    Update App.js

--- App.js
+++ App.js
@@ -25,7 +25,7 @@ const styles = StyleSheet.create({
     flex: 1,    
     backgroundColor: '#ffe4c4', 
     alignItems: 'center', 
-    justifyContent: 'center',
+    justifyContent: 'center', 
     borderRadius: '20',
       
   }, 
