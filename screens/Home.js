6c788bad02f5a2bfc3e6cad6284ab5e7a82c27b9
Simon Zerisenay <81813787+simon-zerisenay@users.noreply.github.com>
2023-04-09 16:39:39 +0400

    Update Home.js

--- screens/Home.js
+++ screens/Home.js
@@ -19,7 +19,7 @@ export default function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff', 
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
     // backgroundColor: 'red',
