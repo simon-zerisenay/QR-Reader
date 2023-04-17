7868a7f32fbd995c6de2f15d0ac7550b491f6a99
Simon Zerisenay <81813787+simon-zerisenay@users.noreply.github.com>
2023-04-18 01:08:29 +0400

    Update Home.js

--- screens/Home.js
+++ screens/Home.js
@@ -6,7 +6,7 @@ import { useNavigation } from '@react-navigation/native';
  
  
  
-export default function Home() { 
+export default function Home() {  
     const navigation = useNavigation();  
  
   return (
