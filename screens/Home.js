4b053a1501512e6e516b8811cd7c603c67e03a52
Simon Zerisenay <81813787+simon-zerisenay@users.noreply.github.com>
2023-04-24 00:21:55 +0400

    Update Home.js

--- screens/Home.js
+++ screens/Home.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
 import { useNavigation } from '@react-navigation/native'; 
+import { useNavigation } from '@react-navigation/native';
  
  
  
