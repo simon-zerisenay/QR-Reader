030b7a8fd30d2532d684f6c714517a954fa09115
Simon Zerisenay <81813787+simon-zerisenay@users.noreply.github.com>
2023-04-24 00:22:34 +0400

    Update Home.js

--- screens/Home.js
+++ screens/Home.js
@@ -1,7 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
-
 import { useNavigation } from '@react-navigation/native';
  
  
