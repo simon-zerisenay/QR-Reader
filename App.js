b9137ff8b3110f41035653c6fd41d2cbc0981d53
Simon Zerisenay <81813787+simon-zerisenay@users.noreply.github.com>
2023-04-26 16:36:53 +0400

    Update App.js

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ function App() {
     <Stack.Navigator> 
       <Stack.Screen name="Home" component={Home} /> 
       <Stack.Screen name="Scanner" component={Scanner} /> 
-    </Stack.Navigator>    
+    </Stack.Navigator>     
   );    
 } 
     
