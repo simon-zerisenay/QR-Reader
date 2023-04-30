427dd97876b08d24da78c5f2e78506d5dcc34911
Simon Zerisenay <81813787+simon-zerisenay@users.noreply.github.com>
2023-04-30 15:54:46 +0400

    Update Home.js

--- screens/Home.js
+++ screens/Home.js
@@ -5,7 +5,7 @@ import { useNavigation } from '@react-navigation/native';
 
 export default function Home() {  
     const navigation = useNavigation();   
-  return (
+  return ( 
     <View style={styles.container}>
         <Button title='Scan' onPress={() => navigation.navigate("Scanner")}/>
     </View> 
