cdf80ea1a35ea1d2f60200fb621900c11457dd54
Simon Zerisenay <81813787+simon-zerisenay@users.noreply.github.com>
2023-05-08 00:08:01 +0400

    Update Home.js

--- screens/Home.js
+++ screens/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
-export default function Home() { 
+export default function Home() {  
     const navigation = useNavigation();   
   return ( 
     <View style={styles.container}>
