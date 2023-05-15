33f54c82c76724830733f75110d681b21eb1c6cf
Simon Zerisenay <81813787+simon-zerisenay@users.noreply.github.com>
2023-05-15 16:23:11 +0400

    Update Scanner.js

--- screens/Scanner.js
+++ screens/Scanner.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Text, View, StyleSheet, Button } from 'react-native';
 import { BarCodeScanner } from 'expo-barcode-scanner';
  
-export default function Scanner() { 
+export default function Scanner() {  
   const [hasPermission, setHasPermission] = useState(null);  
   const [scanned, setScanned] = useState(false);
 
