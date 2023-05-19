0577ab9e027c7e74da3ecbb5f37f604c479f5eda
Simon Zerisenay <81813787+simon-zerisenay@users.noreply.github.com>
2023-05-19 18:15:54 +0400

    Update App.js

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';     
 import { StyleSheet, Text, View, Button } from 'react-native';      
 import Scanner from './screens/Scanner';   
-import { createStackNavigator } from '@react-navigation/stack';    
+import { createStackNavigator } from '@react-navigation/stack';     
 import Home from './screens/Home';       
 import { NavigationContainer } from '@react-navigation/native';       
       
